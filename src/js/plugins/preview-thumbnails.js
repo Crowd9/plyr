3a25479f5516fbc06ef4f4874723287bb8796609
nskazki <nskazki@gmail.com>
2020-03-18 15:44:00 +0300

    gleam - add "ggs" prefix to "plyr__preview-thumb" class

--- src/js/plugins/preview-thumbnails.js
+++ src/js/plugins/preview-thumbnails.js
@@ -294,7 +294,7 @@ class PreviewThumbnails {
      * Create HTML elements for image containers
      */
     render() {
-        // Create HTML element: plyr__preview-thumbnail-container
+        // Create HTML element: ggs-plyr__preview-thumbnail-container
         this.elements.thumb.container = createElement('div', {
             class: this.player.config.classNames.previewThumbnails.thumbContainer,
         });
@@ -320,7 +320,7 @@ class PreviewThumbnails {
             this.player.elements.progress.appendChild(this.elements.thumb.container);
         }
 
-        // Create HTML element: plyr__preview-scrubbing-container
+        // Create HTML element: ggs-plyr__preview-scrubbing-container
         this.elements.scrubbing.container = createElement('div', {
             class: this.player.config.classNames.previewThumbnails.scrubbingContainer,
         });
